50c708e612f39819644bd8b9cefe722cbdd677ba
Keystone25 <marvelkeystone25@gmail.com>
2026-04-03 16:43:44 +0530

    Discover page have been added with new news api.

--- Nutri_Final/static/js/newsfeed.js
+++ Nutri_Final/static/js/newsfeed.js
@@ -1,33 +1,51 @@
 const newsContainer = document.getElementById("news");
 
-
 window.addEventListener("DOMContentLoaded", () => {
-  
-  fetch(`https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=df7b877109524599a7998944ecda0fd6`)
-.then((response) => response.json())
-.then((data) => {
-        
-        newsContainer.innerHTML = "";
+
+  fetch('https://newsdata.io/api/1/latest?apikey=pub_7f4a59bfb11640358b4d9f5587acd24d&country=in&language=en,ml&category=health,food&image=1&removeduplicate=1')
+    .then(response => response.json())
+    .then(data => {
+
+      newsContainer.innerHTML = "";
+
+
+      if (!data.results || data.results.length === 0) {
+        newsContainer.innerHTML = "<p>No fitness/health news found.</p>";
+        return;
+      }
+
+      data.results.forEach(article => {
+
+        const newsCard = document.createElement("div");
+        newsCard.className = "w3-card-4 w3-margin-bottom";
 
         
-        data.articles.forEach((article) => {
-          
-          const newsCard = document.createElement("div");
-          newsCard.className = "w3-card-4 w3-margin-bottom";
-          
-          newsCard.innerHTML = `
-            <img src="${article.urlToImage}" class="w3-image" style="width: 100%" alt="${article.title}">
-            <div class="w3-container w3-padding">
-              <h5 class="w3-text-black">${article.title}</h5>
-              <p class="w3-text-grey">${article.description}</p>
-              <a href="${article.url}" target="_blank" class="w3-button w3-blue-grey">Read more</a>
-            </div>`;
-          
-          
-          newsContainer.appendChild(newsCard);
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+        const image = article.image_url 
+          ? article.image_url 
+          : "https://via.placeholder.com/200x100?text=Fitness+News";
+
+        const description = article.description 
+          ? article.description 
+          : "No description available.";
+
+        const link = article.link;
+
+        newsCard.innerHTML = `
+          <img src="${image}" class="w3-image" style="width:auto; height:auto; display:block; margin:auto;">
+          <div class="w3-container w3-padding">
+            <h5>${article.title}</h5>
+            <p>${description}</p>
+            <a href="${link}" target="_blank" class="w3-button w3-blue-grey">Read more</a>
+          </div>
+        `;
+
+        newsContainer.appendChild(newsCard);
+      });
+
+    })
+    .catch(error => {
+      console.error(error);
+      newsContainer.innerHTML = "<p> Failed to load news</p>";
+    });
+
 });
